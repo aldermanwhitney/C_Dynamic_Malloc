c5fdb2ec61f66dd172c6d5c91abe9b81e19d14f2
Whitney Alderman <wa125@ls.cs.rutgers.edu>
2020-10-14 18:54:50 -0400

    small change

--- mymalloc.c
+++ mymalloc.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 
 /**This file is for our mymalloc and myfree function implementations
- *
- */
+ *There will not be a main in this file, will need to link when compiling
+Compile this with gcc mymalloc.c -c to make it an object file instead of an executeable
+or, with gcc -Wall -Werror mymalloc.c -c
+*/
+
+static char myblock[4096]; //global variable for memory
 
 void mymalloc(){
+printf("myblock[4096] %c", myblock[0]); //this is just here temporarily for compilation
 
 }
 
 void myfree(){
 
-}
-
-//Not sure if we need a main in this file?
-int main(int argc, char** argv){
-
-return 0;
 }

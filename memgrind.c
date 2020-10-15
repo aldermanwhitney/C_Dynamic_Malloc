a2e02f6863cf49074149ee56865c9aac1997ac21
Whitney Alderman <wa125@ls.cs.rutgers.edu>
2020-10-15 11:40:12 -0400

    testA in memgrind and time

--- memgrind.c
+++ memgrind.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 /**This file will include the memory test
  *To test our functions for mymalloc() and myfree()
  from mymalloc.c
  */
 
+void testA(){
+
+struct timeval start_time;
+gettimeofday(&start_time, NULL);
+printf("start_time in seconds: %ld\n", start_time.tv_sec);
+printf("start_time in microseconds: %ld\n", start_time.tv_usec);
+
+
+for (int i = 0; i<120; i++){
+char *ptr = malloc(1);
+free(ptr);
+}	
+	
+
+
+struct timeval end_time;
+gettimeofday(&end_time, NULL);
+printf("end_time: %ld\n", end_time.tv_sec);
+printf("end_time in microseconds: %ld\n", end_time.tv_usec);
+
+printf("elapsed_time: %ld\n", end_time.tv_sec-start_time.tv_sec);
+printf("elapsed_time in microseconds: %ld\n", end_time.tv_usec-start_time.tv_usec);
+}
+
+
+
 int main(int argc, char **argv){
 
-
+testA();
 return 0;
 }

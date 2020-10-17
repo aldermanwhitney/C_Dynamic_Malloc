25ca6b4d29ecc819407388798ac835c0aefd1c55
Whitney Alderman <wa125@ls.cs.rutgers.edu>
2020-10-17 15:49:18 -0400

    working on mymalloc

--- mymalloc.c
+++ mymalloc.c
@@ -21,11 +21,12 @@ struct Metadata *next;
 
 
 void printbyteindex(struct Metadata* metadata){
-
-
 printf("metadata at myblock[%ld:%ld]\n", ((char *)metadata - (char *)&myblock[0]), ((char*) metadata - (char *)&myblock[0])+16);	
 }
 
+void printuserptrindex(void* userptr){
+printf("userptr at myblock[%ld]\n", ((char *)userptr - (char *)&myblock[0]));	
+}
 
 void printlinkedlist(struct Metadata* firstmetadata){
 printf("\n");
@@ -48,7 +49,7 @@ return ((char * )metadata - (char * )&myblock[0]);
 /**Parameters: metadata containing info of block to be split, and number of bytes user needs
  *
  */
-void splitblock(struct Metadata* metadata, int bytes){
+void* splitblock(struct Metadata* metadata, int bytes){
 
 int oldsize = metadata->size;	
 int blockexcess = (metadata->size)-bytes;
@@ -59,6 +60,7 @@ int blockexcess = (metadata->size)-bytes;
 
 //create new block
 if (blockexcess>sizeof(struct Metadata)){
+puts("create new block");
 void* userptr = (void*)metadata + sizeof(struct Metadata) + 1; //pointer to first spot after meta data
 struct Metadata *newmetadata = (void*)&myblock[getindex(metadata)+sizeof(struct Metadata)+bytes]; 
 newmetadata->isfree = 1;
@@ -68,7 +70,9 @@ newmetadata->next = metadata->next;
 metadata->next = newmetadata;
 metadata->size = bytes;
 metadata->isfree = 0;
-//printf("metadata points to byte %ld\n", ((char *)metadata - (char *)&myblock[0]));	
+
+printf("userdata points to byte %ld\n", ((char *)userptr - (char *)&myblock[0]));	
+return userptr;
 }
 
 
@@ -107,12 +111,14 @@ return 1;
 }
 
 
-void mymalloc(int bytes, char* file, int line){
+void* mymalloc(int bytes, char* file, int line){
 
 struct Metadata *firstmetadata = (void*)&myblock[0]; //set a struct metadata pointer pointing for first spot in myblock
 
 printf("\n\nmalloc(%d)\n", bytes);
 printf("is this the first malloc?: %d\n", isfirstmalloc());
+
+
 if (isfirstmalloc()){
 
 //If this is the first call to malloc, create first metadata block	
@@ -121,38 +127,36 @@ firstmetadata->size = 4096-sizeof(struct Metadata);
 firstmetadata->next=NULL;
 
 //dont need this yet
-void* userptr = (void*)firstmetadata + sizeof(struct Metadata) + 1;
+void* userptr2 = (void*)firstmetadata + sizeof(struct Metadata) + 1;
 
 printf("(int)firstmetadata points at myblock[%ld]\n", ((int *)firstmetadata - (int *)&myblock[0]));	
-printf("(int) userptr points at myblock[%ld]\n", ((int *)userptr - (int *)&myblock[0]));	
+printf("(int) userptr points at myblock[%ld]\n", ((int *)userptr2 - (int *)&myblock[0]));	
 printf("(char)firstmetadata points at myblock[%ld]\n", ((char *)firstmetadata - (char *)&myblock[0]));	
-printf("(char) userptr points at myblock[%ld]\n", ((char *)userptr - (char *)&myblock[0]));	
+printf("(char) userptr points at myblock[%ld]\n", ((char *)userptr2 - (char *)&myblock[0]));	
 
 printf("firstmetadata points at byte %ld\n", ((char *)firstmetadata - (char *)&myblock[0]));	
-printf("userptr points at byte %ld\n", ((char *)userptr - (char *)&myblock[0]));	
-//return;
+printf("userptr points at byte %ld\n", ((char *)userptr2 - (char *)&myblock[0]));	
 }
 
 struct Metadata *current = firstmetadata;
 struct Metadata *prev = NULL;
-//while((char*)current<=&myblock[4096]){
-while(current!=NULL){
 
-if ((current->size)>=bytes){
+//iterate through metadata linked list
+//searching for the first metadata block, if any
+//that is large enough and free
+while(current!=NULL){
+if (((current->size)>=bytes)&& (current->isfree==1)){
 printf("found block which matches request. Splitting..\n");
-splitblock(current, bytes);
+void* userptr = splitblock(current, bytes);
 printlinkedlist(firstmetadata);
-return;
+return userptr;
 }		
 prev = current;
 current = current->next;
 }
-//reached the end of the linked list - no block found which is big enough
-if (current==NULL){
-//return NULL
-}	
 
-
+//reached the end of the linked list - no block found which is big enough and free
+return NULL;
 }
 
 
@@ -162,10 +166,14 @@ void myfree(){
 
 int main(){
  
-malloc(5);
-malloc(8);
-malloc(7);	
-//for(int i=0; i<3; i++){	
+char* call1 = malloc(5);
+char* call2 = malloc(8);
+char* call3 = malloc(7);
+
+printuserptrindex(call1);
+printuserptrindex(call2);
+printuserptrindex(call3);
+//for(int i=0; i<240; i++){	
 //malloc(1);
 //}
  // printmemory();
